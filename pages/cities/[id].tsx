7453ce3a31549e6cb16198df2e8ed04f63579b73
Kimbug <kimbug@planetariumhq.com>
2022-04-12 23:38:13 +0900

    🚧 Fetch dummy markers

--- pages/cities/[id].tsx
+++ pages/cities/[id].tsx
@@ -1,27 +1,35 @@
 import type { GetServerSidePropsContext, NextPage } from 'next'
-import GoogleMapReact from 'google-map-react'
+import { ClickEventValue } from 'google-map-react'
 
 import axios from 'utils/axios'
 import { getStringQuery } from 'utils/urls'
+
+import { GoogleMap } from 'components/GoogleMap'
 
 type CityPageProps = {
   id: string
   lat: number
   lng: number
+  markers: Marker[]
   zoom: number
 }
 
-const CityPage: NextPage<CityPageProps> = ({ id, lat, lng, zoom }) => {
+const CityPage: NextPage<CityPageProps> = ({ id, lat, lng, markers, zoom }) => {
+  function onClick({ x, y, lat, lng, event }: ClickEventValue) {
+    console.log(x, y, lat, lng, event)
+  }
+
   return (
     <div>
       City: {id}
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_API_KEY as string }}
-          defaultCenter={{ lat, lng }}
-          defaultZoom={zoom}
-        />
-      </div>
+      <hr />
+      <GoogleMap
+        lat={lat}
+        lng={lng}
+        zoom={zoom}
+        markers={markers}
+        onClick={onClick}
+      />
     </div>
   )
 }
@@ -35,25 +43,33 @@ async function fetchCityCoordinates(city: string) {
     .catch((err) => console.log(err))
 }
 
+async function fetchCityMarkers(city: string) {
+  return axios
+    .get('/markers.json')
+    .then((res) => res?.data && res.data[city])
+    .catch((err) => console.log(err))
+}
+
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const { params } = ctx
   const id = getStringQuery(params?.id)
 
-  const data = await fetchCityCoordinates(id)
+  const city = await fetchCityCoordinates(id)
+  const markers = await fetchCityMarkers(id)
 
-  if (!data) {
+  if (!city) {
     return {
       notFound: true,
     }
   }
 
-  const { lng, lat, zoom } = data
-
+  const { lng, lat, zoom } = city
   return {
     props: {
       id,
       lng,
       lat,
+      markers,
       zoom,
     },
   }
